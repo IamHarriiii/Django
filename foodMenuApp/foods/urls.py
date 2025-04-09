8697c37be4d7f3d2f25f210a828f68c5d6941e4a
IamHarriiii <iamharriiiii@gmail.com>
2025-04-09 06:40:51 +0530

    Studied Template

--- foodMenuApp/foods/urls.py
+++ foodMenuApp/foods/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.GreetUser, name='greet'),
-    path('items/',views.items,name='items')
+    path('items/',views.items,name='items'),
+    path('view/<int:item_id>',views.detailedView,name='dView'),
 ]
--- foodMenuApp/foods/views.py
+++ foodMenuApp/foods/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 from foods.models import Items
 
 def GreetUser(request):
     obj1 = Items.objects.all()
-
-    return HttpResponse(obj1)
+    template = loader.get_template('index.html')
+    context = {
+        'item_list' : obj1,
+    }
+    return render(request,'index.html',context)
 
 def items(request):
     return HttpResponse("this is a items view")
+
+
+def detailedView(request,item_id):
+    item = Items.objects.get(pk=item_id)
+    context = {
+        'item':item,
+    }
+    return render(request,'detail.html',context)
