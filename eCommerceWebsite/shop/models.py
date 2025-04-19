2593887fb1f4b479ad5244d4546adbd6bcc3da83
IamHarriiii <iamharriiiii@gmail.com>
2025-04-19 23:36:40 +0530

    Created Product models

--- eCommerceWebsite/shop/models.py
+++ eCommerceWebsite/shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    product_title = models.CharField(max_length=25)
+    product_price = models.FloatField(default=0.0)
+    product_DiscPrice = models.FloatField(default=0.0)
+    product_category = models.CharField(max_length=50)
+    product_description = models.TextField(max_length=500)
+    product_image = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.product_title
