d4af08eb2c309361a16a97fb815705dd1937191a
IamHarriiii <iamharriiiii@gmail.com>
2025-04-20 08:24:38 +0530

    Added the Search Functionality to ECom Site

--- eCommerceWebsite/shop/views.py
+++ eCommerceWebsite/shop/views.py
@@ -3,8 +3,12 @@ from . models import Products
 # Create your views here.
 
 def index(request):
-    product_objs = Products.objects.all()
-    context ={
-       'product_objs' : product_objs, 
-    }
-    return render(request,'index.html',context=context)
+   itemName = request.GET.get('search')
+   if itemName:
+      product_objs = Products.objects.filter(product_title__icontains=itemName)
+   else:
+      product_objs = Products.objects.all()
+   context = {
+      'product_objs': product_objs,
+   }
+   return render(request, 'index.html', context=context)
