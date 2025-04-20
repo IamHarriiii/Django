d9c4896d68f8bad5e35a28e6bc1969301dca11b6
IamHarriiii <iamharriiiii@gmail.com>
2025-04-20 10:19:34 +0530

    Added Pagination Feature

--- eCommerceWebsite/shop/views.py
+++ eCommerceWebsite/shop/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
-from . models import Products
-# Create your views here.
+from .models import Products
+from django.core.paginator import Paginator
 
 def index(request):
-   itemName = request.GET.get('search')
-   if itemName:
-      product_objs = Products.objects.filter(product_title__icontains=itemName)
-   else:
-      product_objs = Products.objects.all()
-   context = {
-      'product_objs': product_objs,
-   }
-   return render(request, 'index.html', context=context)
+    # Get search parameter
+    itemName = request.GET.get('search')
+    
+    #Search Code
+    if itemName:
+        product_list = Products.objects.filter(product_title__icontains=itemName)
+    else:
+        product_list = Products.objects.all()
+    
+    product_list = product_list.order_by('id')
+    
+    # Paginator Code
+    paginator = Paginator(product_list, per_page=4)
+    pageNum = request.GET.get('page')
+    product_objs = paginator.get_page(pageNum)
+    
+    context = {
+        'product_objs': product_objs,
+        'paginator': paginator,
+    }
+    
+    return render(request, 'index.html', context=context)
